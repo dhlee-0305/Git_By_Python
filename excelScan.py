4fa8824442b08dc7841efbbefb1db93b369eab0a
이대현 <dhlee@gaeasoft.co.kr>
2023-07-12 16:27:07 +0900

    배포 프로그램 목록 파일에서 배포 리스트(경로, 파일명) 로딩 로직 추가

---
+++ excelScan.py
@@ -0,0 +1,21 @@
+import pandas as pd
+from config import *
+
+config = loadConfig()
+
+def pathStrip(path):
+    return path.lstrip('/').lstrip('\\').rstrip('/').rstrip('\\')
+
+def loadExcel():
+    path = config['ENV']['EXCEL_FILE_PATH']
+    data = pd.read_excel(path)
+
+    deployList = []
+    for idx, row in data.iterrows():
+        if idx > 4 and len(row[7]) > 0 :
+            deployFile = [pathStrip(row[7]), row[8]]
+            deployList.append(deployFile)
+            print(deployFile)
+    return deployList
+
+deployList = loadExcel()
--- gitScan.py
+++ gitScan.py
@@ -8,25 +8,32 @@ def connectGit(path):
     repo = Repo(path)
     return repo
 
-def scanChangeFile(repo, prev, curr):
+def scanChangeFile(path, prev, curr):
+    repo = connectGit(path)
+
     prev = repo.commit(prev)
     curr = repo.commit(curr)
     diff_index = prev.diff(curr)
+
+    '''
+    change_type
+        ’A’ for added paths
+        ’D’ for deleted paths
+        ’R’ for renamed paths
+        ’M’ for paths with modified data
+        ’T’ for changed in the type paths
+    '''
     changeFileList = []
     for diff in diff_index:
         fileName = os.path.basename(str(diff.b_path))
         path = os.path.dirname(str(diff.b_path))
         changeFile = [diff.change_type, path, fileName]
-        #print('['+diff.change_type+'] '+path+'/'+fileName)
-        print(changeFile)
 
         changeFileList.append(changeFile)
     return changeFileList
 
-repo_path = config['ENV']['REPO_PATH']
-repository = connectGit(repo_path)
-
-prev_commit = config['COMMIT']['PREV_COMMIT']
-curr_commit = config['COMMIT']['CURR_COMMIT']
-fileList = scanChangeFile(repository, prev_commit, curr_commit)
+repoPath = config['ENV']['REPO_PATH']
+prevCommit = config['COMMIT']['PREV_COMMIT']
+currCommit = config['COMMIT']['CURR_COMMIT']
+commitList = scanChangeFile(repoPath, prevCommit, currCommit)
 
