fcc7da5844366eddf60a03a192649ceb4084a291
이대현 <dhlee@gaeasoft.co.kr>
2023-07-13 17:18:39 +0900

    inner class 채크 기능 추가

--- CheckDeploy.py
+++ CheckDeploy.py
@@ -1,3 +1,5 @@
+import re
+
 from config import *
 from gitScan import *
 from excelScan import *
@@ -19,9 +21,10 @@ def checkDeploy():
     for commit in commitList:
         commitCheck = 'X'
         for deploy in deployList:
-            if compareFile(commit[2], deploy[1]) :
+            if compareFileName(commit[2], deploy[1]) :
                 commitCheck = 'O'
-
+        if isInnerClassExist(repoPath, commit[1], commit[2]):
+            commitCheck = commitCheck + '|I'
         log.debug('['+commitCheck+'] '+ commit[2])
 
 
@@ -31,12 +34,29 @@ def checkDeploy():
     for deploy in deployList:
         deployCheck = 'X'
         for commit in commitList:
-            if compareFile(deploy[1], commit[2]):
+            if compareFileName(deploy[1], commit[2]):
                 deployCheck = 'O'
         
         log.debug('['+deployCheck+'] '+ deploy[1])
+    
+    # printCommit(commitList)
 
-def compareFile(str1, str2):
-    return str1.rstrip('.class').rstrip('.java') == str2.rstrip('.class').rstrip('.java')
+def compareFileName(str1, str2):
+    return re.sub('\$|\d', '', str1).rstrip('.class').rstrip('.java') == re.sub('\$|\d', '', str2).rstrip('.class').rstrip('.java')
+
+def printCommit(commitList):
+    log.debug('----- PRINT COMMIT LIST -----')
+    for commit in commitList:
+        if len(commit[1]) > 0:
+            log.debug('['+commit[0]+'] /'+ commit[1] + '/' + commit[2])
+        else:
+            log.debug('['+commit[0]+'] /'+ commit[2])
+
+def isInnerClassExist(repoPath, filePath, sourceName):
+    path = repoPath + filePath
+    binPath = path.replace('\\', '/').replace('//', '/').replace('/src', '/bin')
+    ret = os.path.isfile(binPath + '/'+sourceName.rstrip('.java')+'$.class') \
+        or os.path.isfile(binPath + '/'+sourceName.rstrip('.java')+'$1.class')
+    return ret
 
 checkDeploy()
